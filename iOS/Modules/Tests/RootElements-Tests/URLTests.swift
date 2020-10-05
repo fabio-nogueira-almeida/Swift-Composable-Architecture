ab4dcd986e9c83268ab688f3e2b503395e38ea59
Fábio Nogueira de Almeida <fna.contact@gmail.com>
2020-10-05 15:01:30 -0300

    change old url

--- iOS/Modules/Tests/RootElements-Tests/URLTests.swift
+++ iOS/Modules/Tests/RootElements-Tests/URLTests.swift
@@ -6,6 +6,6 @@ import XCTest
 class URLTests: XCTestCase {
 
 	func testShouldVerifyIfBaseURLIsCorrect() {
-		XCTAssert(URL.baseURL().absoluteString == "https://enigmatic-bayou-48219.herokuapp.com/api/v2")
+		XCTAssert(URL.baseURL().absoluteString == "https://oneforallapi.herokuapp.com")
 	}
 }
